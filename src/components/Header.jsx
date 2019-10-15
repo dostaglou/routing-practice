82aa42d27798e97bbee82c9588b6f4d37009a140
dillon wyatt <dostaglou@gmail.com>
2019-10-15 21:10:41 +0900

    NavLink established

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-
+import { NavLink } from 'react-router-dom';
+// ActiveStyle={{ background: 'tomato'}}
+// ActiveClassName="my-class-name"
 const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/teachers">Teachers</Link></li>
-      <li><Link to="/Courses">Courses</Link></li>
+      <li><NavLink exact to="/">Home</NavLink></li>
+      <li><NavLink exact to="/about">About</NavLink></li>
+      <li><NavLink exact to="/teachers">Teachers</NavLink></li>
+      <li><NavLink exact to="/Courses">Courses</NavLink></li>
     </ul>
   </header>
 );
